c250ee24a6a87cbdf35915b32f44f845a534af45
Micha Sengotta <micha.sengotta@gmail.com>
2024-02-16 11:49:20 +0100

    Update to Godot 4.1

--- scenes/card/card.gd
+++ scenes/card/card.gd
@@ -5,14 +5,27 @@ class_name Card
 @onready var front_omni_light: OmniLight3D = $FrontOmniLight3D
 @onready var back_omni_light: OmniLight3D = $BackOmniLight3D
 
-var is_face_up: bool = true : set=set_is_face_up
-var is_highlighted: bool = false : set=set_is_highlighted
+@export var is_face_up: bool = true : set=set_is_face_up
+@export var is_highlighted: bool = false : set=set_is_highlighted
+
+
+func _ready():
+	set_is_face_up(is_face_up)
+	set_is_highlighted(is_highlighted)
 
 
 func set_is_face_up(value: bool):
-	if is_face_up == value:
-		return
 	is_face_up = value
+	
+	var target_rotation: float = 0.0 if is_face_up else PI
+	print(rotation.z)
+	if rotation.z == target_rotation:
+		return
+	
+	if not anim_player: # in editor
+		rotation.z = target_rotation
+		return
+	
 	if is_face_up:
 		anim_player.play_backwards("flip")
 	else:
